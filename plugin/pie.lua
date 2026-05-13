ea4bdfb1dbc8f347431f24c971534017146cefec
QΛSIM <qasimstatic@gmail.com>
2026-05-13 21:50:39 +0530

    feat: highlight groups and plugin loader

---
+++ plugin/pie.lua
@@ -0,0 +1,4 @@
+-- pie.nvim: highlights
+-- Links to Keyword by default so it works with any colorscheme
+vim.api.nvim_set_hl(0, "PiePromptIcon", { link = "Keyword" })
+vim.api.nvim_set_hl(0, "PieSpinner", { link = "Keyword" })
